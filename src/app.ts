16f685e852618c978234c938007a9d9af08afb3d
manik <maniksarker652@gmail.com>
2024-11-10 15:26:22 +0600

    create funsction for upload csv file

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,11 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import router from './app/routes';
 import notFound from './app/middlewares/notFound';
 const app: Application = express();
-
+import multer from 'multer';
+import uploadCsvFile from './app/helper/uploadCsvFile';
+import auth from './app/middlewares/auth';
+import { USER_ROLE } from './app/modules/user/user.constant';
+const upload = multer({ dest: 'uploads/' });
 // parser
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +27,8 @@ app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(express.static('uploads'));
 // application routers ----------------
 app.use('/', router);
+
+app.post("/upload-csv",auth(USER_ROLE.superAdmin), upload.single('file'),uploadCsvFile)
 
 // global error handler
 app.use(globalErrorHandler);
---
+++ src/app/helper/uploadCsvFile.ts
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import fs, { PathLike } from 'fs';
+import csv from 'csv-parser';
+import League from '../modules/league/league.model';
+import Team from '../modules/team/team.model';
+import Player from '../modules/player/player.model';
+const uploadCsvFile = async (req: Request, res: Response) => {
+  const filePath = req?.file?.path;
+  const results: any = [];
+
+  fs.createReadStream(filePath as PathLike)
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', async () => {
+      try {
+        for (const row of results) {
+          const {
+            leagueName,
+            leagueImage,
+            sport,
+            teamName,
+            teamLogo,
+            teamBgImage,
+            playerName,
+            playerPosition,
+            playerBgImage,
+            playerImage,
+          } = row;
+
+          // Find or create League
+          let league = await League.findOne({ name: leagueName });
+          if (!league) {
+            league = new League({
+              name: leagueName,
+              league_image: leagueImage,
+              sport,
+            });
+            await league.save();
+          }
+
+          // Find or create Team
+          let team = await Team.findOne({ name: teamName, league: league._id });
+          if (!team) {
+            team = new Team({
+              name: teamName,
+              team_logo: teamLogo,
+              team_bg_image: teamBgImage,
+              league: league._id,
+            });
+            await team.save();
+          }
+
+          let player = await Player.findOne({
+            name: playerName,
+            position: playerPosition,
+            team: team._id,
+          });
+
+          if (!player) {
+            // Create Player
+            player = new Player({
+              name: playerName,
+              league: league._id,
+              team: team._id,
+              position: playerPosition,
+              player_image: playerImage,
+              player_bg_image: playerBgImage,
+            });
+            await player.save();
+          }
+        }
+
+        // Delete the file after processing
+        fs.unlinkSync(filePath as PathLike);
+
+        res.status(200).send('Data successfully uploaded and saved.');
+      } catch (error) {
+        res.status(500).send(`Error processing data`);
+      }
+    });
+};
+
+export default uploadCsvFile;
