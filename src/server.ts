66c2d74fb65557f2931a1312b24ec208a9818c5e
manik <maniksarker652@gmail.com>
2024-12-15 09:03:39 +0600

    s

--- src/server.ts
+++ src/server.ts
@@ -16,10 +16,7 @@ async function main() {
 
     const port =
       typeof config.port === 'number' ? config.port : Number(config.port);
-    // myServer = server.listen(port, config.base_url as string, () => {
-    //   logger.info(`Example app listening on port ${config.port}`);
-    //   seedSuperAdmin();
-    // });
+
     myServer = server.listen(port, '0.0.0.0' as string, () => {
       logger.info(`Example app listening on port ${config.port}`);
       seedSuperAdmin();
