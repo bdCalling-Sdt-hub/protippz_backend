0964b2b68251c631a0e581d94409b995326a2998
manik <maniksarker652@gmail.com>
2025-05-17 10:32:04 +0600

    s

--- src/app/constant/index.ts
+++ src/app/constant/index.ts
@@ -1,4 +1,4 @@
 export const maxDistanceForShop = 3000; // distance meter
 export const riderSpeed = 10; // meter per second
-export const inviteRewardPoint = 100;
+export const inviteRewardPoint = 10;
 export const pointPerAmountTip = 10;
